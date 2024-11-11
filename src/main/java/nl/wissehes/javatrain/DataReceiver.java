c530b1f09f221b21a3c50e1484322dc01c33152a
Wisse Hes <wisse@wissehes.nl>
2024-11-11 17:46:14 +0100

    Add XML data parsing

--- src/main/java/nl/wissehes/javatrain/DataReceiver.java
+++ src/main/java/nl/wissehes/javatrain/DataReceiver.java
@@ -1,7 +1,9 @@
 package nl.wissehes.javatrain;
 
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import nl.wissehes.javatrain.model.reisinformatie.DepartureRoot;
 import org.springframework.stereotype.Component;
 import org.zeromq.ZMQ;
 
@@ -14,7 +16,10 @@ import java.util.zip.GZIPInputStream;
 
 @Component
 public class DataReceiver {
-    public static List<String> receivedMessages = new ArrayList<>();
+    public static List<DepartureRoot> receivedMessages = new ArrayList<>();
+    public static List<String> receivedMessagesRaw = new ArrayList<>();
+
+    private final XmlMapper mapper = new XmlMapper();
 
     private final ZMQ.Socket subscriber;
 
@@ -30,9 +35,13 @@ public class DataReceiver {
                 byte[] messageBytes = subscriber.recv(); // Receive the compressed message as bytes
 
                 try {
+                    System.out.println("Received message on topic: " + topic);
+
                     String message = decompress(messageBytes);
-                    receivedMessages.add(message);
-                    System.out.println("Received message on topic: " + topic + " - " + message);
+                    receivedMessagesRaw.add(message);
+
+                    DepartureRoot departureRoot = parseXML(message);
+                    receivedMessages.add(departureRoot);
                 } catch (IOException e) {
                     System.err.println("Failed to decompress message: " + e.getMessage());
                 }
@@ -57,6 +66,15 @@ public class DataReceiver {
             }
 
             return byteArrayOutputStream.toString("UTF-8"); // Convert to string with appropriate encoding
+        }
+    }
+
+    private DepartureRoot parseXML(String xml) {
+        try {
+            return mapper.readValue(xml, DepartureRoot.class);
+        } catch (Exception e) {
+            System.err.println("Failed to parse XML: " + e.getMessage());
+            return null;
         }
     }
 }
