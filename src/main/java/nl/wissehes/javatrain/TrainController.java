a538c2d96e565065fc4c8c11c24c6cb84746620b
Wisse Hes <wisse@wissehes.nl>
2024-11-12 21:03:08 +0100

    Update TrainController to use the DataStore

--- src/main/java/nl/wissehes/javatrain/TrainController.java
+++ src/main/java/nl/wissehes/javatrain/TrainController.java
@@ -2,23 +2,48 @@ package nl.wissehes.javatrain;
 
 import nl.wissehes.javatrain.mapper.DepartureMapper;
 import nl.wissehes.javatrain.model.departure.Departure;
+import nl.wissehes.javatrain.model.response.DeparturesResponse;
+import nl.wissehes.javatrain.model.shared.Station;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TrainController {
 
+    private final DataStore dataStore = DataStore.getInstance();
+
     @GetMapping(value = "/received", produces = "application/json")
     public Departure getReceivedMessages() {
-        var departure = DataReceiver.receivedMessages.getLast();
-
-        DepartureMapper mapper = new DepartureMapper(departure);
-        return mapper.mapDeparture();
+        return dataStore.getDepartures().getLast();
     }
 
     @GetMapping(value = "/received/raw", produces = "application/xml")
     public String getReceivedMessagesRaw() {
-        return DataReceiver.receivedMessagesRaw.getLast();
+        return dataStore.getRawDepartures().getLast();
+    }
+
+    @GetMapping(value = "/departures/{station}", produces = "application/json")
+    public DeparturesResponse getDepartures(@PathVariable String station) {
+        var stationData = dataStore.getStations()
+                .stream()
+                .filter(s -> s.code.equalsIgnoreCase(station))
+                .findFirst()
+                .orElse(null);
+
+        var departures = dataStore.getDepartures()
+                .stream()
+                .filter(d -> d.forStation.equals(stationData))
+                .toList();
+
+        return new DeparturesResponse(stationData, departures);
+    }
+
+    @GetMapping(value = "/stations", produces = "application/json")
+    public List<Station> getStations() {
+        return dataStore.getStations();
     }
 
 }
