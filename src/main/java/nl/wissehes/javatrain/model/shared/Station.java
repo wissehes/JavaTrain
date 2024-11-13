93510f6f0b4eefea9d19c0b71487fe94c35fc4d1
Wisse Hes <wisse@wissehes.nl>
2024-11-13 13:50:34 +0100

    Add station type enum

--- src/main/java/nl/wissehes/javatrain/model/shared/Station.java
+++ src/main/java/nl/wissehes/javatrain/model/shared/Station.java
@@ -1,9 +1,11 @@
 package nl.wissehes.javatrain.model.shared;
 
+import nl.wissehes.javatrain.model.departure.ScheduleChange;
+
 public class Station {
 
     public String code;
-    public int type;
+    public StationType type;
     public int uicCode;
 
     public String shortName;
@@ -27,7 +29,7 @@ public class Station {
 
     public Station(String code, int type, int uicCode, String shortName, String mediumName, String longName) {
         this.code = code;
-        this.type = type;
+        this.type = StationType.fromCode(type);
         this.uicCode = uicCode;
         this.shortName = shortName;
         this.mediumName = mediumName;
@@ -36,10 +38,40 @@ public class Station {
 
     public Station(nl.wissehes.javatrain.model.NDOV.Station station) {
         this.code = station.code;
-        this.type = Integer.parseInt(station.type);
+        this.type = StationType.fromCode(Integer.parseInt(station.type));
         this.uicCode = Integer.parseInt(station.uicCode);
         this.shortName = station.korteNaam;
         this.mediumName = station.middelNaam;
         this.longName = station.langeNaam;
+    }
+
+    public enum StationType {
+        LOCAL_STATION(0),
+        LOCAL_STATION_JUNCTION(1),
+        EXPRESS_STATION(2),
+        EXPRESS_STATION_JUNCTION(3),
+        INTERCITY_STATION(4),
+        INTERCITY_STATION_JUNCTION(5),
+        MEGA_STATION(6),
+        FACULTATIVE_STATION(7);
+
+        private final int code;
+
+        StationType(int code) {
+            this.code = code;
+        }
+
+        public static StationType fromCode(int code) {
+            for (StationType mod : values()) {
+                if (mod.code == code) {
+                    return mod;
+                }
+            }
+            return FACULTATIVE_STATION;
+        }
+
+        public int getCode() {
+            return code;
+        }
     }
 }
