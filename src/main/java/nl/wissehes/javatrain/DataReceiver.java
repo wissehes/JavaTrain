ace5725594f9b24fdfbb722e1283aca3c79c1d95
Wisse Hes <wisse@wissehes.nl>
2024-11-13 16:31:40 +0100

    Use logger

--- src/main/java/nl/wissehes/javatrain/DataReceiver.java
+++ src/main/java/nl/wissehes/javatrain/DataReceiver.java
@@ -3,6 +3,8 @@ package nl.wissehes.javatrain;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import nl.wissehes.javatrain.util.GZipUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.zeromq.ZMQ;
 
@@ -12,6 +14,7 @@ import java.io.IOException;
 public class DataReceiver {
 
     private final ZMQ.Socket subscriber;
+    Logger logger = LoggerFactory.getLogger(DataReceiver.class);
 
     public DataReceiver(ZMQ.Socket subscriber) {
         this.subscriber = subscriber;
@@ -25,12 +28,12 @@ public class DataReceiver {
                 byte[] messageBytes = subscriber.recv(); // Receive the compressed message as bytes
 
                 try {
-                    System.out.println("Received message on topic: " + topic);
+                    logger.debug("Received message on topic: {}", topic);
 
                     String message = GZipUtils.decompress(messageBytes);
                     DataStore.getInstance().addDeparture(message);
                 } catch (IOException e) {
-                    System.err.println("Failed to decompress message: " + e.getMessage());
+                    logger.error("Failed to decompress message: {}", e.getMessage());
                 }
             }
         }).start();
