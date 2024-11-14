bd1e44231d113dbff784e615e97eb2d4281856db
Wisse Hes <wisse@wissehes.nl>
2024-11-14 21:35:34 +0100

    Throw not found error

--- src/main/java/nl/wissehes/javatrain/controller/DeparturesController.java
+++ src/main/java/nl/wissehes/javatrain/controller/DeparturesController.java
@@ -2,7 +2,9 @@ package nl.wissehes.javatrain.controller;
 
 import nl.wissehes.javatrain.DataStore;
 import nl.wissehes.javatrain.model.response.DeparturesResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Comparator;
 
@@ -18,7 +20,7 @@ public class DeparturesController {
 
     @GetMapping(value = "/{station}", produces = "application/json")
     public DeparturesResponse getDepartures(@PathVariable String station) {
-        var stationData = dataStore.getStation(station).orElseThrow();
+        var stationData = dataStore.getStation(station).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Station not found"));
 
         var departures = dataStore.getDepartures()
                 .stream()
