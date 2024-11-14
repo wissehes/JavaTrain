7dccfe186b636133738699474e570e6020ac8abe
Wisse Hes <wisse@wissehes.nl>
2024-11-14 20:25:02 +0100

    Add separate variables for dvs/das/rit topics

--- src/main/java/nl/wissehes/javatrain/ZmqConfig.java
+++ src/main/java/nl/wissehes/javatrain/ZmqConfig.java
@@ -2,6 +2,7 @@ package nl.wissehes.javatrain;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
@@ -9,14 +10,21 @@ import org.zeromq.ZMQ;
 public class ZmqConfig {
 
     private static final String ZMQ_SUBSCRIBER_ADDRESS = "tcp://pubsub.besteffort.ndovloket.nl:7664";
-    private static final String TOPIC = "/RIG/InfoPlusDVSInterface4";
+
+    /** Departures */
+    public static final String DVS_TOPIC = "/RIG/InfoPlusDVSInterface4";
+    /** Arrivals */
+    public static final String DAS_TOPIC = "/RIG/InfoPlusDASInterface4";
+    /** Services */
+    public static final String RIT_TOPIC = "/RIG/InfoPlusRITInterface5";
 
     @Bean(destroyMethod = "close")
     public ZMQ.Socket zmqSubscriber() {
         ZContext context = new ZContext();
-        ZMQ.Socket subscriber = context.createSocket(ZMQ.SUB);
+        ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
         subscriber.connect(ZMQ_SUBSCRIBER_ADDRESS);
-        subscriber.subscribe(TOPIC.getBytes(ZMQ.CHARSET)); // Subscribe to the topic
+        subscriber.subscribe(DVS_TOPIC.getBytes(ZMQ.CHARSET)); // Subscribe to the topic
+
         return subscriber;
     }
 }
