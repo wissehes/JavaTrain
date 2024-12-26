568ffd65a11cda4fec7bfff4abb0d15ab3df72e5
Wisse Hes <wisse@wissehes.nl>
2024-12-26 15:37:24 +0100

    Update journey mapper to use Movement

--- src/main/java/nl/wissehes/javatrain/mapper/JourneyMapper.java
+++ src/main/java/nl/wissehes/javatrain/mapper/JourneyMapper.java
@@ -1,6 +1,6 @@
 package nl.wissehes.javatrain.mapper;
 
-import nl.wissehes.javatrain.model.NDOV.InfoStatus;
+import nl.wissehes.javatrain.model.NDOV.*;
 import nl.wissehes.javatrain.model.NDOV.RIT.JourneyDocument;
 import nl.wissehes.javatrain.model.NDOV.RIT.LogischeRit.DeelStation;
 import nl.wissehes.javatrain.model.NDOV.RIT.LogischeRit.RitDeel;
@@ -8,8 +8,13 @@ import nl.wissehes.javatrain.model.NDOV.RIT.ReisInformatieProductRitInfo;
 import nl.wissehes.javatrain.model.NDOV.RIT.RitInfo;
 import nl.wissehes.javatrain.model.journey.Journey;
 import nl.wissehes.javatrain.model.journey.JourneyPart;
+import nl.wissehes.javatrain.model.journey.Movement;
 import nl.wissehes.javatrain.model.journey.Stop;
+import nl.wissehes.javatrain.model.shared.ScheduleChange;
 import nl.wissehes.javatrain.model.shared.Station;
+
+import java.time.Duration;
+import java.util.List;
 
 public class JourneyMapper {
 
@@ -46,7 +51,10 @@ public class JourneyMapper {
         var part = new JourneyPart();
 
         part.serviceNumber = deel.deelNummer;
-        part.stops = deel.stops.stream().map(this::mapJourneyPartStop).toList();
+        part.stops = deel.stops.stream()
+                .filter(i -> i.stopStatus.stream().anyMatch(s -> s.stopStatus() == NSBoolean.J))
+                .map(this::mapJourneyPartStop)
+                .toList();
 //        part.changes = deel.wijzigingen.stream().map(ScheduleChange::new).toList();
         return part;
     }
@@ -59,8 +67,18 @@ public class JourneyMapper {
             stop.recognizableDestination = new Station(station.herkenbareBestemming.station());
         }
 
-        stop.isStationAccessible = station.stationToegankelijk.toBoolean();
-        stop.isAssistanceAvailable = station.stationReisAssistentie.toBoolean();
+        if(station.stationToegankelijk != null) {
+            stop.isStationAccessible = station.stationToegankelijk.toBoolean();
+        } else stop.isStationAccessible = false;
+
+        if(station.stationReisAssistentie != null) {
+            stop.isAssistanceAvailable = station.stationReisAssistentie.toBoolean();
+        } else stop.isAssistanceAvailable = false;
+
+
+        if(station.wijzigingen != null) {
+            stop.changes = station.wijzigingen.stream().map(ScheduleChange::new).toList();
+        }
 
         if(station.eindBestemming != null) {
             var foundActualStation = station.eindBestemming.stream().filter(e -> e.InfoStatus == InfoStatus.ACTUEEL).findFirst();
@@ -71,13 +89,47 @@ public class JourneyMapper {
         }
 
         if(station.aankomstTijd != null) {
-            var foundActualArrival = station.aankomstTijd.stream().filter(e -> e.infoStatus() == InfoStatus.ACTUEEL).findFirst();
-            var foundPlannedArrival = station.aankomstTijd.stream().filter(e -> e.infoStatus() == InfoStatus.GEPLAND).findFirst();
+            stop.arrival = mapMovement(station.aankomstTijd, station.treinAankomstSpoor, station.exacteAankomstVertraging, station.gedempteAankomstVertraging, station.wijzigingen);
+        }
 
-            foundActualArrival.ifPresent(aankomstTijd -> stop.actualArrivalTime = aankomstTijd.date());
-            foundPlannedArrival.ifPresent(aankomstTijd -> stop.plannedArrivalTime = aankomstTijd.date());
+        if(station.vertrekTijd != null) {
+            stop.departure = mapMovement(station.vertrekTijd, station.treinVertrekSpoor, station.exacteVertrekVertraging, station.gedempteVertrekVertraging, station.wijzigingen);
         }
 
         return stop;
+    }
+
+    private Movement mapMovement(List<Trein.VertrekTijd> tijd, List<TreinSpoor> spoor, String exactDelay, String normalizedDelay, List<Wijziging> wijzigingen) {
+        var movement = new Movement();
+
+        if(tijd != null) {
+            var foundActualArrival = tijd.stream().filter(e -> e.infoStatus() == InfoStatus.ACTUEEL).findFirst();
+            var foundPlannedArrival = tijd.stream().filter(e -> e.infoStatus() == InfoStatus.GEPLAND).findFirst();
+
+            foundActualArrival.ifPresent(aankomstTijd -> movement.actualTime = aankomstTijd.date());
+            foundPlannedArrival.ifPresent(aankomstTijd -> movement.plannedTime = aankomstTijd.date());
+        }
+
+        if(spoor != null) {
+            var foundActualPlatform = spoor.stream().filter(e -> e.infoStatus == InfoStatus.ACTUEEL).findFirst();
+            var foundPlannedPlatform = spoor.stream().filter(e -> e.infoStatus == InfoStatus.GEPLAND).findFirst();
+
+            foundActualPlatform.ifPresent(aankomstSpoor -> movement.actualPlatform = aankomstSpoor.getValue());
+            foundPlannedPlatform.ifPresent(aankomstSpoor -> movement.plannedPlatform = aankomstSpoor.getValue());
+        }
+
+        if(exactDelay != null) {
+            movement.exactDelay = Duration.parse(exactDelay).getSeconds();
+        }
+
+        if(normalizedDelay != null) {
+            movement.normalizedDelay = Duration.parse(normalizedDelay).toMinutes();
+        }
+
+        if(wijzigingen != null && wijzigingen.stream().anyMatch(w -> w.wijzigingType == 32)) {
+            movement.cancelled = true;
+        }
+
+        return movement;
     }
 }
