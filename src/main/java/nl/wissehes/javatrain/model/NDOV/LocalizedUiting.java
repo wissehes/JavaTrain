48bbbc6fc60a8f91653d53acf0611cc3e4569f16
Wisse Hes <wisse@wissehes.nl>
2025-04-29 10:00:46 +0200

    feat: Add method to get uiting for specific method

--- src/main/java/nl/wissehes/javatrain/model/NDOV/LocalizedUiting.java
+++ src/main/java/nl/wissehes/javatrain/model/NDOV/LocalizedUiting.java
@@ -4,14 +4,27 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 import java.util.List;
+import java.util.Optional;
 
 public class LocalizedUiting {
     @JacksonXmlProperty(localName = "Uitingen")
     @JacksonXmlElementWrapper(useWrapping = false)
     public List<Uiting> uitingen;
 
+    public Optional<String> getForLanguage(Language language) {
+        return uitingen
+                .stream()
+                .filter(u -> u.language.equals(language))
+                .findFirst()
+                .map(u -> u.text);
+    }
+
     public record Uiting(
             @JacksonXmlProperty(localName = "Uiting") String text,
-            @JacksonXmlProperty(localName = "Taal", isAttribute = true) String language
+            @JacksonXmlProperty(localName = "Taal", isAttribute = true) Language language
     ){}
+
+    public enum Language {
+        nl, en
+    }
 }
